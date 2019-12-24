10575a3c1fd55a070045b8a796a4d35403f0e845
oleksandr.malykhin <oleksandr.malykhin@nure.ua>
2019-12-24 20:01:00 +0200

    End stage 7

--- UM/src/main/java/UM/malykhin/nure/agent/SearchAgent.java
+++ UM/src/main/java/UM/malykhin/nure/agent/SearchAgent.java
@@ -1,8 +1,24 @@
 package UM.malykhin.nure.agent;
 
+import UM.malykhin.nure.db.DaoFactory;
+import UM.malykhin.nure.db.DatabaseException;
 import jade.core.Agent;
 
+import java.sql.SQLException;
+import java.util.Collection;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
 public class SearchAgent extends Agent {
+	
+	private AID[] aids;
+	private SearchGui gui = null;
 	
 	protected void setup() {
 		super.setup();
@@ -12,6 +28,24 @@ public class SearchAgent extends Agent {
 	protected void takeDown() {
 		System.out.println(getAID().getName() + " terminated");
 		super.takeDown();
+	}
+	
+	public void search(String firstName, String lastName) throws SQLException {
+		try {
+			Collection users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+			if (users.size() > 0) {
+				showUsers(users);
+			} else {
+				addBehaviour(new SearchRequestBehaviour(aids, firstName, lastName));
+			}
+		} catch (DatabaseException e) {
+			throw new SQLException(e);
+		}
+
+	}
+	
+	void showUsers(Collection user) {
+		gui.addUsers(user);
 	}
 	
 }
